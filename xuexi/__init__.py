439b40f029d1576f90c6cf7eadee5958e4b84b41
yangzuoming <65052194+yangzuoming@users.noreply.github.com>
2020-09-27 12:32:03 +0800

    2020年9月27日，补货阅读文章列表中的错误，减少出错概率

--- xuexi/__init__.py
+++ xuexi/__init__.py
@@ -925,7 +925,11 @@ class App(Automation):
                 logger.debug(f'真是遗憾，一屏都没有可点击的新闻')
                 articles = []
             for article in articles:
-                title = article.get_attribute("name")
+                try:
+                    title = article.get_attribute("name")
+                except Exception as ex:
+                    logger.info(ex)
+                    continue
                 if title in self.titles:
                     continue
                 try:
@@ -1084,35 +1088,36 @@ class App(Automation):
         logger.debug(f'正在进行新闻学习...')
         # 找指定的新闻阅读频道
         self.safe_click('//*[@resource-id="cn.xuexi.android:id/home_bottom_tab_button_work"]')
-        vol_not_found = True
-        while vol_not_found:
-            # 顶多右划4次，找不到就返回
-            right_slide = 3
-            while right_slide >= 0:
-                try:
-                    volumns = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, rules['article_volumn'])))
-                # volumns = self.find_elements(rules['article_volumn'])
-                except:
-                    self.safe_back('mine -> home')
-                    volumns = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, rules['article_volumn'])))
-                first_vol = volumns[1]
-                for vol in volumns:
-                    title = vol.get_attribute("name")
-                    logger.debug(title)
-                    if self.volumn_title == title:
-                        vol.click()
-                        # 找到约定栏目，标记退出循环
-                        vol_not_found = False
-                        right_slide = -2
-                        break
-                else:
-                    logger.debug(f'未找到 {self.volumn_title}，右划')
-                    # self.safe_click(rules['article_share'])
-                    # self.safe_back('mine -> home')
-                    self.driver.scroll(vol, first_vol, duration=500)
-                    right_slide = right_slide - 1
-            else:
-                self.safe_click('//*[@resource-id="cn.xuexi.android:id/home_bottom_tab_button_work"]')
+        self._get_article_vol()
+        # vol_not_found = True
+        # while vol_not_found:
+        #     # 顶多右划4次，找不到就返回
+        #     right_slide = 3
+        #     while right_slide >= 0:
+        #         try:
+        #             volumns = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, rules['article_volumn'])))
+        #         # volumns = self.find_elements(rules['article_volumn'])
+        #         except:
+        #             self.safe_back('mine -> home')
+        #             volumns = self.wait.until(EC.presence_of_all_elements_located((By.XPATH, rules['article_volumn'])))
+        #         first_vol = volumns[1]
+        #         for vol in volumns:
+        #             title = vol.get_attribute("name")
+        #             logger.debug(title)
+        #             if self.volumn_title == title:
+        #                 vol.click()
+        #                 # 找到约定栏目，标记退出循环
+        #                 vol_not_found = False
+        #                 right_slide = -2
+        #                 break
+        #         else:
+        #             logger.debug(f'未找到 {self.volumn_title}，右划')
+        #             # self.safe_click(rules['article_share'])
+        #             # self.safe_back('mine -> home')
+        #             self.driver.scroll(vol, first_vol, duration=500)
+        #             right_slide = right_slide - 1
+        #     else:
+        #         self.safe_click('//*[@resource-id="cn.xuexi.android:id/home_bottom_tab_button_work"]')
         # self.safe_back('mine -> home')
         # self._kaleidoscope()
         self.safe_click('//*[@resource-id="cn.xuexi.android:id/home_bottom_tab_button_work"]')
